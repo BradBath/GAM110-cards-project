b726769b11cab16f08647651e9729f30e4e37997
Bradley Bath <badfitz66@gmail.com>
2019-11-07 12:57:51 +0000

    Added Tower.cs to Assets/Scripts/Abstract.
    It's the base class all tower's should inherit from.

---
+++ Assets/Scripts/Abstract/Tower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game.Abstract
+{
+    //Base class for towers. All tower types should inherit from this. This is the strategy design pattern.
+    public abstract class Tower : MonoBehaviour
+    {
+
+        //An abstract function means that whatever inherits from this class MUST implement this function via public override void OnPlacement.
+        //This function is also just a test. We need to figure out what functions each tower should have (shoot, placement, mouse down, mouse hover, mouse up, etc)
+        public abstract void OnPlacement();
+    }
+}
