0a29f468b08891d32543af29dce2cda32430e88b
BradBath <badfitz66@gmail.com>
2019-11-08 14:05:49 +0000

    Whoops. Forgot to add files. (see previous commit for changes and additions)

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Abstract;
+public class Mouse : MonoBehaviour
+{
+    //This script handles mouse interaction with towers (such as hovering and clicking). Should be attached to camera.
+    Tower currentlyHovering; //Keep track of what we are currently hovering so we can call MouseHoverOff when we stop hovering it.
+    [SerializeField] //This serialized the variable so it can be displayed in the inspector without being a public variable
+    Camera mainCamera; // Main camera of the scene (Camera.main is slow)
+    void Update()
+    {
+        Ray mouseRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(mouseRay, out hit))
+        {
+            if (hit.transform && hit.transform.GetComponent<Tower>())
+            {
+                Tower towerController = hit.transform.GetComponent<Tower>();
+                if (towerController == currentlyHovering)
+                    return;
+
+                towerController.MouseHoverOn();
+                if (currentlyHovering)
+                {
+                    currentlyHovering.MouseHoverOff();
+                }
+                currentlyHovering = towerController;
+            }
+            else if (!hit.transform || !hit.transform.GetComponent<Tower>())
+            {
+                currentlyHovering.MouseHoverOff();
+                currentlyHovering = null;
+            }
+        }
+        else
+        {
+            if (currentlyHovering)
+            {
+                currentlyHovering.MouseHoverOff();
+                currentlyHovering = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Towers/RomanArcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Abstract;
+public class RomanArcher : Tower
+{
+    
+    /*ToDo list:
+     * Create a menu system for towers.
+     * menu system will show stats for towers (damage, firing speed) and upgrading 
+     * menus can be pinned (meaning that they will stay in place and can be interacted with) but also have a sort-of preview on hover which will disappear when the mouse stops hovering the object.
+    */
+    public override void MouseDown()
+    {
+        //Pin upgrade menu so you can interact with it
+        throw new System.NotImplementedException();
+    }
+
+    public override void MouseHoverOff()
+    {
+        //Shows upgrade menu 
+        print("Hovered off.");
+    }
+    public override void MouseHoverOn()
+    {
+        //Hides upgrade menu (if not pinned)
+        print("Hovered on.");
+    }
+
+    public override void MouseUp()
+    {
+        //Not sure what to put here.
+        throw new System.NotImplementedException();
+    }
+    void Update()
+    {
+        //Do update code (enemy detection, shooting)
+    }
+}
