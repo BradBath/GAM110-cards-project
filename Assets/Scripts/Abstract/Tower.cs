2e4bcf8c9aa455de7402f7da1424e2230e5372bb
BradBath <badfitz66@gmail.com>
2019-11-08 14:03:39 +0000

    Changes: Changed the functions inside Tower.cs
    Added:
    Plans for a UI system
    Added the first tower type: Roman Archer
    A script to handle mouse interaction for towers (Mouse.cs)

--- Assets/Scripts/Abstract/Tower.cs
+++ Assets/Scripts/Abstract/Tower.cs
@@ -8,9 +8,9 @@ namespace Game.Abstract
     //Base class for towers. All tower types should inherit from this. This is the strategy design pattern.
     public abstract class Tower : MonoBehaviour
     {
-
-        //An abstract function means that whatever inherits from this class MUST implement this function via public override void OnPlacement.
-        //This function is also just a test. We need to figure out what functions each tower should have (shoot, placement, mouse down, mouse hover, mouse up, etc)
-        public abstract void OnPlacement();
+        public abstract void MouseDown();
+        public abstract void MouseHoverOff();
+        public abstract void MouseHoverOn();
+        public abstract void MouseUp();
     }
 }
