8457e5f9cb3966eb4b275b4c7c6f8c884a032c77
Sam Hendrickx <samhendrickx93@gmail.com>
2019-11-13 21:06:03 +0100

    Added enemy scripts and pathfinding scripts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target1, target2;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target1 = PathPoints.points[0];
+        //target2 = PathPoints2.points2[0];
+    }
+
+    void Update()
+    {
+        //if (spawnpoint = 1)
+        //{
+            Vector2 direction = target1.position - transform.position;
+            transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+            if (Vector2.Distance(transform.position, target1.position) <= 0.2f)
+            {
+                GetNextPath();
+            }
+        //}
+        //else
+        /*{
+            Vector2 direction = target2.position - transform.position;
+            transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+            if (Vector2.Distance(transform.position, target2.position) <= 0.2f)
+            {
+                GetNextPath();
+            }
+        }*/
+    }
+
+     void GetNextPath()
+    {
+        if (wavepointIndex >= PathPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target1 = PathPoints.points[wavepointIndex];
+    }
+}
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject[] enemiePrefabs;
+    public Transform[] spawnPoints;
+
+    public float timeBetweenEnemies = 5f;
+    private float countdown = 2f;
+    private int wavenumber = 1;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnEnemy();
+            countdown = timeBetweenEnemies;
+        }
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnEnemy ()
+    {
+        for (int i = 0; i < wavenumber; i++)
+        {
+            SpawnCharacter();
+        }
+        wavenumber++;
+    }
+       
+    //<3 From Christopher with Love
+    void SpawnCharacter()
+    {
+        int rndEnemy = Random.Range(0, enemiePrefabs.Length);
+        int rndSpawn = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemiePrefabs[rndEnemy], spawnPoints[rndSpawn].position, Quaternion.identity);
+    }
+}
